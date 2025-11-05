4f6ceebf1d4edd9bfe235d9da11d035de9679a4c
espinalclark <7clarkespinal@gmail.com>
2025-11-05 15:01:17 -0500

    subiendo htb_ports

--- config/bin/htb_ports.sh
+++ config/bin/htb_ports.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-TARGET_FILE="$HOME/.config/bin/target"
-
-if [ ! -r "$TARGET_FILE" ]; then
-    echo "ﲅ No target"
-    exit 0
-fi
-
-read -r ip_target name_target _ < "$TARGET_FILE"
-
-if [ -n "$ip_target" ] && [ -n "$name_target" ]; then
-    echo "什 $ip_target - $name_target"
-elif [ -n "$ip_target" ]; then
-    echo "什 $ip_target"
+# Verifica si el archivo existe
+if [ -f ~/.config/bin/ports ]; then
+    PORTS=$(cat ~/.config/bin/ports | tr ' ' '|')
+    if [ -n "$PORTS" ]; then
+        echo "⛩  $PORTS"
+    else
+        echo "⛩  No ports"
+    fi
 else
-    echo "ﲅ No target"
+    echo "⛩  No ports"
 fi
 
