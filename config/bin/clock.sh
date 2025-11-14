f37a96fb58e22992df512136f185f5e6ef4e1829
espinalclark <7clarkespinal@gmail.com>
2025-11-13 23:03:29 -0500

    subiendo bin

---
+++ config/bin/clock.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Obtiene la fecha y hora actual
+date_formatted=$(date +"%b %I:%M %p")
+tooltip_date=$(date +"%Y-%m-%d")
+
+# Icono del reloj (puedes cambiarlo por , , , etc.)
+icon=""
+
+# Muestra solo el icono y la hora
+echo "$icon $date_formatted"
+
